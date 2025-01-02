94b2af0a423a05858b364a1669d1eb3963d1c79f
Tomba-Bobmanuel <tomba.bobmanuel@gmail.com>
2025-01-02 12:46:03 +0100

    practice-8

---
+++ week-6/practice-8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\pc\Desktop\t.bobmanuelCSC101\week-6\practice-8\target\debug\deps\practice_8.exe: src/main.rs
+
+C:\Users\pc\Desktop\t.bobmanuelCSC101\week-6\practice-8\target\debug\deps\practice_8.d: src/main.rs
+
+src/main.rs:
