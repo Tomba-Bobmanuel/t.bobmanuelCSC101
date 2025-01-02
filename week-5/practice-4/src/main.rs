e98878d6a8d48247b7e1fee1af12eaf2c1dd1b49
Tomba-Bobmanuel <tomba.bobmanuel@gmail.com>
2025-01-02 10:21:36 +0100

    practice-5

--- week-5/practice-4/src/main.rs
+++ week-5/practice-4/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+
+    let fullname = "Chibudum John Umeh";
+    let department = " Computer Science";
+    let uni = "Pan-Atlantic University";
+
+
+    let mut school = "School of Science".to_strings();
+    //push strings
+    school.push_str("and Technology");
+
+    println!("My name is: {}", fullname);
+    //check length
+    println!("The length my fullname is: {}",fullname.len());
+    println!("I am a student of {} Department",deprament);
+    println!("{}",school);
+    println!("{}",uni);
 }
---
+++ week-5/practice-5/target/debug/practice-5.d
@@ -0,0 +1,1 @@
+C:\Users\pc\Desktop\t.bobmanuelCSC101\week-5\practice-5\target\debug\practice-5.exe: C:\Users\pc\Desktop\t.bobmanuelCSC101\week-5\practice-5\src\main.rs
